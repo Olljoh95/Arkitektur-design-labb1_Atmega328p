b8d557b0f989b29d6511a6413c4838c0fc6dafeb
Olle J <user@LAPTOP-CRHEC88J.localdomain>
2021-10-25 16:11:55 +0200

    Updated uart_init with proper handling of UCSR0C register. Comments updated for comprehension

--- serial.c
+++ serial.c
@@ -5,32 +5,29 @@
 #include "led.h"
 
 //Sida 185 megaAVR® Data Sheet
-#define FOSC 16000000 //Clock speed
-#define BAUDRATE 38400 //Baudrate
-#define BAUD_PRESCALE FOSC/16/BAUDRATE -1 //calculation for presacling
+#define FOSC 16000000                        //Clock speed
+#define BAUDRATE 38400                      //Baudrate
+#define BAUD_PRESCALE FOSC/16/BAUDRATE -1  //calculation for presacling
 
 void uart_init(void) {
-    //Set baud rate, two ways.
-    //uint16_t ubrrn = FOSC/(16*BAUD) - 1;
-
-        //Set them seperatly
+    //Baud rate set seperatly
     UBRR0H = (BAUD_PRESCALE >> 8); 
     UBRR0L = BAUD_PRESCALE;
-    //Or set them both at the same time
-    //UBRR0 = ubrrn;
 
-        //UCSR0B = USART Control and Status Register B
-        //RXEN = Enable receiver, TXEN0 = enable transmitter
+    //UCSR0B = USART Control and Status Register B
+    //RXEN = Enable receiver, TXEN0 = enable transmitter
     UCSR0B = (1<<RXEN0) | (1<<TXEN0);
 
-        //The first and second bit of the USR0 register C
-        //sets the number of data bits in a frame
-        //the recviever and transmitter will use.
-        //In this case UCSZ00 = 3 (8bit), USB0 = 1 (2 stopbit)
-    UCSR0C = (1 << USBS0) | (3 << UCSZ00);
+    //UCSR0C = USART control and status register C
+    //I will set character size, parity, and UART/USART Mode, and number of stop bits to 8N1
+    UCSR0C = (1 << UCSZ00) | (1 << UCSZ01); //8 bit character size
 
-    //Set parity to none
-    //UCSR0C &= ~_BV(UPM01) & ~_BV(UPM00); //Also set to zero by default, also redundant
+    UCSR0C |= (0 << USBS0); //1 stop bit
+
+    UCSR0C |= (0 << UPM00) | (0 << UPM01); //No parity
+
+    UCSR0C |= (0 << UMSEL00) | (0 << UMSEL01); // Asynchronous USART/UART mode
+
 }
 
 void uart_putchar(unsigned char chr) {
